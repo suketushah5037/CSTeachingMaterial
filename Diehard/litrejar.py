2b906ba50a9aa59e0d592649225cb87433749b62
sahirvsahirv <gowri.r@gmail.com>
2017-08-29 15:47:13 +0530

    diehar puzzle

---
+++ Diehard/litrejar.py
@@ -0,0 +1,125 @@
+#3,6,9
+#3,6,8
+#2,5,1
+
+#Diophantine equations are equations where only integral solutions are required
+
+#linear diophantine equation - same way where there are integral values
+
+#it has a property - gcd(a,b) is divisible by c
+#ax+by =  c
+
+#gcd(a,b) should be divisible by c
+
+#Given two integers aa and bb:
+
+#the Euclidean Algorithm computes GCD(a,b)
+#the Extended Euclidean Algorithm gives out 2 integers xx and yy such that ax+by=GCD(a,b); mainly used when GCD(a,b)=1
+#GCD is one they are coprime
+
+#Use extended euclid's method for solving gcd
+
+#Use it to check c % GCD(a,b) == 0
+
+#Die hard problem - 3 litre and a 5 litre jar and retieve 5 litre jar
+
+#using code
+#5, 3, 4
+#5, 0
+#5-3=2, 0+3     temp = 5, 3-0 = 3
+#2,0
+#0,2                temp = 2, 3-0 = 2
+#5, 2
+#4, 3                temp = 5, 3-2 = 1
+
+#intuition
+#0, 0
+#5,0
+#2,3
+#2,0
+#0,2
+#5,2
+#4,3
+
+
+def gcd(a,b):
+    if(a == 0):
+        return b
+    return gcd(b%a, a)
+
+def isPossible(a,b,c):
+    #print(a, b, c)
+    greatestdivisor = gcd(a,b)
+    print("gcd=",greatestdivisor)
+    if( (c % greatestdivisor) == 0):
+        #print("divisible")
+        return True
+    else:
+        #print("not divisible")
+        return False
+
+def swap(a,b):
+    temp = b
+    b = a
+    a = temp
+    
+#3,6,9
+#3,6,8
+#2,5,1
+
+def pour(fromContainer,toContainer,required):
+    print(fromContainer,toContainer,required)
+    #Fill the from container
+    fromtemp =  fromContainer
+    totemp = 0
+    print(fromtemp, totemp)
+    #Do till required quantity of 4 is not arrived
+    while( (fromtemp != required) and (totemp != required)):
+        #Add or subtract the
+        #minimum of the liquid in fromtemp and totemp-toContainer
+        temp = min(fromtemp, toContainer - totemp)
+        #print(temp)
+        fromtemp = fromtemp - temp
+        totemp =  temp + totemp
+        print(fromtemp, totemp)
+        #If either has reacher the required - stop
+        if((fromtemp == required) or (totemp == required)):
+            print("Success")
+            break
+        #If the from container is empty fill it up
+        if(fromtemp == 0):
+            fromtemp = fromContainer
+        #If the to container is full, empty it
+        if(totemp == toContainer):
+            totemp = 0
+        print(fromtemp, totemp)
+        
+a = 3
+b = 5
+c = 4
+
+#a = 18
+#b = 9
+#c = 2
+
+#print("calculating feasibility")
+possible = False
+possible = isPossible(a,b,c)
+#print(possible)
+if(possible == False):
+    print("not possible to solve the puzzle")
+else:
+    #maintain the lesser liquid in a
+    if(a>b):
+        swap(a,b)
+        print("swapping")
+    if(c > b):
+        print("Wrong scenario, output is greater than the input")
+    else:
+        print("required = ", c)
+        print("from", "to")
+        pour(a,b,c)
+    
+
+
+
--- Python_CG/DP_Pascal'sFib/pascals.py
+++ Python_CG/DP_Pascal'sFib/pascals.py
@@ -1,5 +1,167 @@
+#Pascal's triangle works on nCk formula
 #16C3 = 16*15*14/3*2*1
 
+
+#Method 1 - Calculate each location using the formula n!(k!*(n-k)!)
+import math
+x = math.factorial(4)
+print("Factorial libary function in python")
+print(x)
+
+#What if you do not have the library function?
+
+#Method 2 - pattern recognition - each row item is the sum of previous adjacent row items in the above row
+#Maintain a two dimensional array
+#i is the row and j is the column
+
+NOOFROWS = 5
+print("Pattern recognition method")
+pascals = []
+oldlist = []
+i = 1
+while(i<=NOOFROWS):
+    #Initialize the one dimensional array for the rows that get added to the two dimensional array
+    mylist = [None]*i
+    #The first item is always 1
+    mylist[0]=1
+
+    j = 1
+    print(i)
+
+    if(i > 2):
+        while(j < i-1): 
+            #print(oldlist)
+            mylist[j] = oldlist[j-1] + oldlist[j]
+            j= j + 1
+    #The last item is always 1
+    mylist[i-1] = 1
+    #Append the row to the 2 dimensional array
+    pascals.append(mylist)
+    #print(mylist)
+    #store the previous list in oldlist before forming the new one
+    oldlist = mylist
+    i = i + 1
+    #print(i)
+
+print("Pascal's triangle")
+print(pascals)
+
+##############################################################################
+
+#With recursion
+import sys
+#The recursive function
+def rowitempascal(i, j):
+        #print((i,j))
+        #Base conditions
+        if(j == 0):
+            return(1)
+        elif(j == (i-1)):
+            return(1)
+        else:
+           #Recognise the pattern and form it
+           return(rowitempascal(i-1, j-1) + rowitempascal(i-1, j))
+
+
+
+i = 1
+print("pascals with recursion")
+#use sys.stdout so that the space is added instead of a default '\n' with print
+while(i <= NOOFROWS):
+       j = 0
+        while(j <= i - 1):
+            #so that it does not ome by default with a \n
+            sys.stdout.write(str(rowitempascal(i, j)) + ' ')
+            j = j + 1
+        print("\n")
+        i = i + 1
+                
+
+##############################################################################
+
+#Dynamic programming -
+#memoization and tabulation
+#Examples - Give other examples
+
+#Pascals with memoization - store the value
+#tabulation- store and build from bottom up
+
+
+import sys
+def rowitempascal(i, j):
+        #print((i,j))
+        if(j == 0):
+            pascals[i][j] = 1
+            return(1)
+        elif(j == (i-1)):
+            pascals[i][j] = 1
+            return(1)
+        elif(pascals[i][j] != -1):
+            #If in cache return from the cache
+            return pascals[i][j]
+        else:
+            #Else recurse
+            pascals[i][j] = rowitempascal(i-1, j-1) + rowitempascal(i-1, j)
+            return(pascals[i][j])
+
+
+
+i = 1
+print("pascals with memoization")
+#For storing already calculated values
+pascals = [None]
+while(i <= 5):
+        #For storing
+        mylist = [-1]*i
+        pascals.append(mylist)
+        
+        j = 0
+        while(j <= i - 1):
+            #so that it does not ome by default with a \n
+            #sys.stdout.write(str(rowitempascal(i, j)) + ' ')
+            rowitempascal(i, j)
+            j = j + 1
+        #print("\n")
+        i = i + 1
+                
+print(pascals)
+
+##############################################################################
+
+#Tabulation - build from bottom up
+
+i = 1
+
+print("pascals with tabulation ")
+#No recursion - calculate from bottom up
+#For storing
+pascals = [None]
+#Directly store it and use it while building up the triangle
+while(i <= 5):
+        #For storing
+        mylist = [-1]*i
+        pascals.append(mylist)
+        j = 0
+        while(j <= i - 1):
+            #so that it does not ome by default with a \n
+            if(j == 0):
+                pascals[i][j] = 1
+            if(j == (i-1)):
+                pascals[i][j] = 1
+            else:
+                pascals[i][j] = pascals[i-1][j-1] + pascals[i-1][j]
+            j = j + 1
+        #print("\n")
+        i = i + 1
+print(pascals)
+
+##############################################################################
+#Exercise
+#Try fibonacci series for the pattern based approcah, recursion, memoization and tabulation
+#Other classic dynamic programming exercises
+
+
+#how the nCk formula is derived
 #16 * 15 * 14 = choose 3 items from 16
 #4 balls RBGY choose 3 - no repetition
 # RB RG RY RR
@@ -27,139 +189,3 @@
 #Mehtod 1
 #RGB RGY RBY GBY - combinations without repetitions
 #nC3 = n!/(n-r)! * r!
-import math
-
-x = math.factorial(4)
-#print(x)
-
-#Method 2 - pattern recognition
-pascals = []
-oldlist = []
-i = 1
-while(i<=5):
-    mylist = [None]*i
-    mylist[0]=1
-    j = 1
-    print(i)
-    if(i > 2):
-        while(j < i-1): 
-            #print(oldlist)
-            mylist[j] = oldlist[j-1] + oldlist[j]
-            j= j + 1
-    mylist[i-1] = 1
-    pascals.append(mylist)
-    print(mylist)
-    oldlist = mylist
-    i = i + 1
-    #print(i)
-
-print("Pascal's triangle")
-print(pascals)
-
-##############################################################################
-
-#With recursion
-import sys
-def rowitempascal(i, j):
-        #print((i,j))
-        if(j == 0):
-            return(1)
-        elif(j == (i-1)):
-            return(1)
-        else:
-           return(rowitempascal(i-1, j-1) + rowitempascal(i-1, j))
-
-
-
-i = 1
-print("pascals with recursion")
-while(i <= 5):
-       
-        
-        j = 0
-        while(j <= i - 1):
-            #so that it does not ome by default with a \n
-            sys.stdout.write(str(rowitempascal(i, j)) + ' ')
-            j = j + 1
-        print("\n")
-        i = i + 1
-                
-
-##############################################################################
-
-#Dynamic programming -
-#memoization and tabulation
-#Examples - Give other examples
-
-#Pascals with memoization - store the value
-#tabulation- store and build from bottom up
-
-
-import sys
-def rowitempascal(i, j):
-        #print((i,j))
-        
-        if(j == 0):
-            pascals[i][j] = 1
-            return(1)
-        elif(j == (i-1)):
-            pascals[i][j] = 1
-            return(1)
-        elif(pascals[i][j] != -1):
-            return pascals[i][j]
-        else:
-            pascals[i][j] = rowitempascal(i-1, j-1) + rowitempascal(i-1, j)
-            return(pascals[i][j])
-
-
-
-i = 1
-print("pascals with memoization")
-#For storing
-pascals = [None]
-while(i <= 5):
-        #For storing
-        mylist = [-1]*i
-        pascals.append(mylist)
-        
-        j = 0
-        while(j <= i - 1):
-            #so that it does not ome by default with a \n
-            #sys.stdout.write(str(rowitempascal(i, j)) + ' ')
-            rowitempascal(i, j)
-            j = j + 1
-        #print("\n")
-        i = i + 1
-                
-print(pascals)
-
-##############################################################################
-
-#Tabulation - build from bottom up
-
-i = 1
-print("pascals with tabulation ")
-#For storing
-pascals = [None]
-while(i <= 5):
-        #For storing
-        mylist = [-1]*i
-        pascals.append(mylist)
-        j = 0
-        while(j <= i - 1):
-            #so that it does not ome by default with a \n
-            if(j == 0):
-                pascals[i][j] = 1
-            if(j == (i-1)):
-                pascals[i][j] = 1
-            else:
-                pascals[i][j] = pascals[i-1][j-1] + pascals[i-1][j]
-            j = j + 1
-        #print("\n")
-        i = i + 1
-print(pascals)
-
-##############################################################################
-#Exercise
-#Try fibonacci series for the pattern based approcah, recursion, memoization and tabulation
-#Other classic dynamic programming exercises
