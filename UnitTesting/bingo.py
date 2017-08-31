0d24d160180d84192e2c0f45831bfcaf7f6e9bce
sahirvsahirv <gowri.r@gmail.com>
2017-08-31 13:59:29 +0530

    TDD and unit testing

--- UnitTesting/bingo.py
+++ UnitTesting/bingo.py
@@ -1,4 +1,5 @@
-#This is what we want. how does TDD drive mosularity, see below
+import pydoc
+"""This is what we want. how does TDD drive mosularity, see below
 
 ##bingoarr = ['b', 'i', 'n', 'g', 'o']
 ##
@@ -8,24 +9,25 @@
 ##    bingoarr[i] = 'clap'
 ##    print(bingoarr)
 ##    i=i+1
+"""
 
 #Article for "self" - https://pythontips.com/2013/08/07/the-self-variable-in-python-explained/
 
-#Class for all aspects for printing BingoRhyme
+"""Class for all aspects for printing BingoRhyme"""
 class BingoGame:
     #variables declared here are class variables
     
-    #constructor to initialize the member variables
+    """constructor to initialize the member variables"""
     def __init__(self):
         self.bingoarr = ['b', 'i', 'n', 'g', 'o']
         self.printstr = []
-    #Function to get the length of the array - which is always 5 in our case
+    """Function to get the length of the array - which is always 5 in our case"""
     def bingoarrlen(self):
         return len(self.bingoarr)
-    #Function to return an element in the array given the index
+    """Function to return an element in the array given the index"""
     def bingoarray(self, i):
         return self.bingoarr[i]
-    #Function to store the rhyme in printstr
+    """Function to store the rhyme in printstr"""
     def bingorhyme(self):
         i = 0
         #This is a reference
@@ -41,10 +43,10 @@ class BingoGame:
             self.printstr.append(printstr)
             printstr = []
         #print(self.printstr)
-    #private function directive to get the printable string - can be accessed outside
+    """private function directive to get the printable string - can be accessed outside"""
     def __getprintstr__(self):
         return self.printstr
-    #Print the string
+    """Print the string"""
     def printbingorhyme(self):
         print(self.__getprintstr__())
 
